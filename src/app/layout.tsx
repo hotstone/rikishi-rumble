3207d13a19bc4e135c30ce3e7c7fd5ad499eb010
Marc <hello@mc7h.me>
2026-05-24 15:00:50 +1000

    New favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,13 @@ export const metadata: Metadata = {
   title: "Rikishi Rumble",
   description: "Sumo wrestling tipping competition",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "16x16" },
+      { url: "/favicon.png", type: "image/png", sizes: "32x32" },
+      { url: "/favicon-48.png", type: "image/png", sizes: "48x48" },
+    ],
+  },
 };
 
 export default function RootLayout({
